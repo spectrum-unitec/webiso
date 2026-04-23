0fd9b23f5f687a2475252455b860a11a743797c3
ari <arihasan7779@gmail.com>
2026-04-23 14:30:22 +0700

    buat header list doc vdf view

--- app/Views/Fe/view_pdf.php
+++ app/Views/Fe/view_pdf.php
@@ -29,7 +29,7 @@
             <div class="col-xl-3">
                 <nav id="sidebar-bootstrap" class="navbar navbar-sticky d-none d-xl-block">
                     <nav class="nav">
-                        <h6 class="d-flex" style="padding: .1875rem 1.5rem;">List Dokumen</h6>
+                        <h5 class="d-flex" style="padding: .1875rem 1.5rem;">List Dokumen</h5>
 
                         <?php
                         $segments = current_url(true)->getSegments();
